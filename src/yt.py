dfb9aec0f2b6f81f208be74e97d3af587cfcfcad
Arnaud Kohler <arnaudikohler@gmail.com>
2026-03-09 14:13:39 -0400

    added searchByTopic func and ran in main.py

--- src/yt.py
+++ src/yt.py
@@ -1,3 +1,16 @@
 from src.config import YOUTUBE_API_KEY
+from googleapiclient.discovery import build
 
-print(YOUTUBE_API_KEY)
+def searchByTopic(topic, max_results):
+    youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+    request = youtube.search().list(
+        q=topic,
+        part="snippet",
+        type="video",
+        order="viewCount",
+        maxResults=max_results
+    )
+
+    response = request.execute()
+
+    return response
