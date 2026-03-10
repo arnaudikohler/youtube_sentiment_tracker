2c01d1147b8240feab0b47ef3ed5cd0e875f0c79
Arnaud Kohler <arnaudikohler@gmail.com>
2026-03-10 11:13:57 -0400

    added captionConverter func and tested it main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from src.yt import searchByTopic
 from src.yt import extract
 from src.yt import captionExtractor
+from src.yt import captionConverter
 import json
 
 def main():
     with open("data/test_search_response.json") as f:
         response = json.load(f)
     
+    with open("data/captions.json", "r") as f2:
+        content = [json.loads(line) for line in f2]
+    
     out = extract(response)
-    out2 = captionExtractor(out)
-    print(out2)
+    print(out)
+    out2 = captionConverter(content)
+    for i in out2:
+        print("------------------")
+        print("------------------")
+        print("------------------")
+        print(i)
 
 if __name__ == "__main__":
     main()
