c304cc507173897f1f9cb73d781e9325a6b7031f
Arnaud Kohler <arnaudikohler@gmail.com>
2026-03-20 23:52:56 -0400

    plotted mean_sentiment over time for nvda & btc

--- src/plots.py
+++ src/plots.py
@@ -8,11 +8,9 @@ conn.close()
 
 df_sentiment["date"] = pd.to_datetime(df_sentiment["date"])
 
-# Filter by topic
 df_topicbtc = df_sentiment[df_sentiment["topic"] == "bitcoin"].copy()
 df_topicnvda = df_sentiment[df_sentiment["topic"] == "nvidia"].copy()
 
-# Filter by one weighting scheme
 df_topicbtc = df_topicbtc[
     (df_topicbtc["title_weight"] == 0.9) &
     (df_topicbtc["description_weight"] == 0.1) &
@@ -25,18 +23,15 @@ df_topicnvda = df_topicnvda[
     (df_topicnvda["caption_weight"] == 0.0)
 ].copy()
 
-# Sort by date
 df_topicbtc = df_topicbtc.sort_values("date")
 df_topicnvda = df_topicnvda.sort_values("date")
 
-# Print data for checking
 print("Bitcoin:")
 print(df_topicbtc[["date", "topic", "sentiment_mean"]])
 
 print("\nNVDA:")
 print(df_topicnvda[["date", "topic", "sentiment_mean"]])
 
-# Plot Bitcoin
 plt.figure(figsize=(10, 5))
 plt.plot(df_topicbtc["date"], df_topicbtc["sentiment_mean"], marker="o")
 plt.xlabel("Date")
@@ -47,7 +42,6 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# Plot NVDA
 plt.figure(figsize=(10, 5))
 plt.plot(df_topicnvda["date"], df_topicnvda["sentiment_mean"], marker="o")
 plt.xlabel("Date")
